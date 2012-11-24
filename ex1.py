28601476e823b2ec5344baa327c51b389ac3d567
Swrangsar Basumatary <swrangsar@aol.com>
2012-11-24 14:23:11 +0530

    oop left to do

---
+++ ex1.py
@@ -0,0 +1,10 @@
+#print "Hello World!"
+# print "Hello Again"
+# print "I like typing this."
+#print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+#print "LOL"
+
+print "I could have code like this." # and the comment after is ignored
